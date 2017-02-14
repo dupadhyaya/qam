6e6861427df66131979648f1df0cb709a1fed4da
dupadhyaya <dup1966@gmail.com>
2017-02-14 19:00:37 +0530

    5

--- lp/a1.R
+++ lp/a1.R
@@ -1,11 +1,18 @@
+library(lpSolve)
+library(lpSolveAPI)
 lps.model <- make.lp(4, 0)
 x <- c(6,2,4,9)
 lps.model
 add.column(lps.model, x)
+lps.model
+
 y <- c(3,1,5)
 ind <- c(1,2,4)
 add.column(lps.model, y, ind)
+lps.model
+
 dimnames(lps.model) <- list(c("alpha", "bravo", "charlie", "delta"),
                             c("whiskey", "tango"))
 lps.model
 delete.lp(lps.model)
+lps.model
